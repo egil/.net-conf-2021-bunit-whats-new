b93742582f5938fd6fc87859484789a1b050f188
Egil Hansen <egil@assimilated.dk>
2021-11-08 14:22:40 +0000

    error boundary example

---
+++ test/Sample.Tests/_Imports.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Components.Routing;
+global using Sample.App;
+global using Sample.App.Shared;
+global using Sample.App.Services;
